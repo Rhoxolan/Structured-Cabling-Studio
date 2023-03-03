ff26894d038f2469cd2bc2da352c5032039b4b43
Rhoxolan <pba17mail@gmail.com>
2023-03-03 12:02:41 +0200

    Add project files.

---
+++ Structured Cabling Studio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StructuredCablingStudio.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
