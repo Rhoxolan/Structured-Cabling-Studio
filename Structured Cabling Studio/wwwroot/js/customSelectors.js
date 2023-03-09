b7a9a959adbd09768f95459006cb9d0ca77d706f
Rhoxolan <pba17mail@gmail.com>
2023-03-09 20:41:09 +0200

    Selector name from custom selectors lib was renamed; verticalSiteContent selector from custom selectors lib was changed

--- Structured Cabling Studio/wwwroot/js/customSelectors.js
+++ Structured Cabling Studio/wwwroot/js/customSelectors.js
@@ -1,10 +1,10 @@
-$('.siteContent').css('height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+$('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
 
 $(window).on('resize', function () {
-    $('.siteContent').css('height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+    $('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
 });
 
-$('.siteContent').on('resize', function () {
-    $(this).css('height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+$('.verticalSiteContent').on('resize', function () {
+    $(this).css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
 });
 
