1b28a2a6e270cecda81d90312e8e01ca360fe7de
Rhoxolan <pba17mail@gmail.com>
2023-03-09 20:34:44 +0200

    Made and optimized left nav panel

--- Structured Cabling Studio/wwwroot/js/customSelectors.js
+++ Structured Cabling Studio/wwwroot/js/customSelectors.js
@@ -1,1 +1,10 @@
-$('.siteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+$('.siteContent').css('height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+
+$(window).on('resize', function () {
+    $('.siteContent').css('height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+});
+
+$('.siteContent').on('resize', function () {
+    $(this).css('height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+});
+
