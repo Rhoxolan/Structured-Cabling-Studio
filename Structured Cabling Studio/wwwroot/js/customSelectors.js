37e3646906a43bf00e9f266a9d7c4e3728d2e8c5
Rhoxolan <pba17mail@gmail.com>
2023-04-08 20:15:28 +0300

    Edited layout page markup

--- Structured Cabling Studio/wwwroot/js/customSelectors.js
+++ Structured Cabling Studio/wwwroot/js/customSelectors.js
@@ -1,27 +1,27 @@
-////.verticalSiteContent
+//.verticalSiteContent
 
-//$('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+$('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
 
-//$(window).on('resize', function () {
-//    $('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
-//});
+$(window).on('resize', function () {
+    $('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+});
 
-//$(window).on('load', function () {
-//    $('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
-//    console.log("Hello from load");
-//});
+$(window).on('load', function () {
+    $('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+    console.log("Hello from load");
+});
 
-//$('footer').on('resize', function () {
-//    $('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
-//});
+$('footer').on('resize', function () {
+    $('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+});
 
-//$('header').on('resize', function () {
-//    $('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
-//});
+$('header').on('resize', function () {
+    $('.verticalSiteContent').css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+});
 
-//$('.verticalSiteContent').on('resize', function () {
-//    $(this).css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
-//});
+$('.verticalSiteContent').on('resize', function () {
+    $(this).css('min-height', $(window).height() - $('header').outerHeight() - $('footer').outerHeight());
+});
 
 
 //.eMailNavDisplay
@@ -31,13 +31,16 @@ $('.eMailNavDisplay').css('font-size', function () {
     if (textLength > 127 && textLength <= 255) {
         return 1.9;
     }
-    else if (textLength > 63) {
+    else if (textLength > 63) { //Max 127
         return 3.8;
     }
-    else if (textLength > 31) {
-        return 7.1;
+    else if (textLength > 45) { //Max 63
+        return 7;
     }
-    else if (textLength > 0) {
+    else if (textLength > 31) { //Max 45
+        return 9.1;
+    }
+    else if (textLength > 0) { //Max 31
         return 15;
     }
 });
