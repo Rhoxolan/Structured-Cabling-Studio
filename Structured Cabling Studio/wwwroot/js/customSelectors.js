b9bdd6200a29b52996366715b6257e5b370d9920
Rhoxolan <pba17mail@gmail.com>
2023-04-18 17:25:08 +0300

    Edited the pattern of the brand display

--- Structured Cabling Studio/wwwroot/js/customSelectors.js
+++ Structured Cabling Studio/wwwroot/js/customSelectors.js
@@ -54,12 +54,32 @@ function resizeBrandDisplay() {
 };
 
 function getBrandDisplayFS() {
-    if ($(window).width() < 576) {
-        return 17;
-    }
-    else {
+    let windowWidth = $(window).width()
+    if (windowWidth > 575) {
         return 25;
     }
-
-    //Сделать лесенку ифэлсеиф и уменьшить fs для экранов меньше, например, 360-и
+    if (windowWidth > 399) {
+        return 19;
+    }
+    if (windowWidth > 359) {
+        return 17;
+    }
+    if (windowWidth > 319) {
+        return 14.5;
+    }
+    if (windowWidth > 299) {
+        return 13.1;
+    }
+    if (windowWidth > 279) {
+        return 11.5;
+    }
+    if (windowWidth > 259) {
+        return 9.5
+    }
+    if (windowWidth > 239) {
+        return 8
+    }
+    if (windowWidth > 0) {
+        return 7.3;
+    }
 }
