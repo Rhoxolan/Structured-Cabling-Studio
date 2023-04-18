8fa8c844e4ade052dcb58de66c426391792af9e5
Rhoxolan <pba17mail@gmail.com>
2023-04-18 10:16:21 +0300

    Changed email display pattern

--- Structured Cabling Studio/wwwroot/js/customSelectors.js
+++ Structured Cabling Studio/wwwroot/js/customSelectors.js
@@ -21,16 +21,16 @@ function getverticalSiteContentSize() {
 $('.eMailNavDisplay').css('font-size', function () {
     let textLength = $(this).prop("innerText").length;
     if (textLength > 127 && textLength <= 255) {
-        return 1.9;
+        return 2;
     }
     else if (textLength > 63) { //Max 127
-        return 3.8;
+        return 4;
     }
     else if (textLength > 45) { //Max 63
-        return 7;
+        return 8;
     }
     else if (textLength > 31) { //Max 45
-        return 9.1;
+        return 12;
     }
     else if (textLength > 0) { //Max 31
         return 15;
