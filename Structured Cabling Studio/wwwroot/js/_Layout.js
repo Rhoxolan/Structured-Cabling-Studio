31ff552ae98723059cf349e33c7c6b12ad2a87db
Rhoxolan <pba17mail@gmail.com>
2023-04-18 20:17:02 +0300

    Edited styles

--- Structured Cabling Studio/wwwroot/js/_Layout.js
+++ Structured Cabling Studio/wwwroot/js/_Layout.js
@@ -2,6 +2,7 @@
 
 let navLinks = document.querySelectorAll('.navLinkText');
 
+verticalSiteNavbarUlFSChanger();
 document.addEventListener('load', verticalSiteNavbarUlFSChanger);
 window.addEventListener('resize', verticalSiteNavbarUlFSChanger);
 window.addEventListener('load', verticalSiteNavbarUlFSChanger);
@@ -9,7 +10,7 @@ navLinks.forEach(n => n.addEventListener('load', verticalSiteNavbarUlFSChanger))
 
 function verticalSiteNavbarUlFSChanger() {
     if (window.innerWidth > 1399) {
-        navLinks.forEach(n => n.style.fontSize = '16px');
+        navLinks.forEach(n => n.style.fontSize = '15.5px');
     }
     else if (window.innerWidth > 575) {
         navLinks.forEach(n => n.style.fontSize = '15px');
@@ -18,7 +19,29 @@ function verticalSiteNavbarUlFSChanger() {
         navLinks.forEach(n => n.style.fontSize = '12px');
     }
     else if (window.innerWidth > 0) {
-        navLinks.forEach(n => n.style.fontSize = '8px');
+        navLinks.forEach(n => n.style.fontSize = '8.1px');
+    }
+};
+
+//#languageSelect
+
+let select = document.getElementById('languageSelect');
+
+languageSelectFSChanger();
+document.addEventListener('load', languageSelectFSChanger);
+window.addEventListener('resize', languageSelectFSChanger);
+window.addEventListener('load', languageSelectFSChanger);
+select.addEventListener('load', languageSelectFSChanger);
+
+function languageSelectFSChanger() {
+    if (window.innerWidth > 575) {
+        select.style.fontSize = '14px';
+    }
+    else if (window.innerWidth > 319) {
+        select.style.fontSize = '12px';
+    }
+    else if (window.innerWidth > 0) {
+        select.style.fontSize = '8px';
     }
 };
 
@@ -27,6 +50,7 @@ function verticalSiteNavbarUlFSChanger() {
 
 let footer = document.getElementById('siteFooterText');
 
+footerFSChanger();
 document.addEventListener('load', footerFSChanger);
 window.addEventListener('resize', footerFSChanger);
 window.addEventListener('load', footerFSChanger);
