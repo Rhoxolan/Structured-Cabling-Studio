69e9038958375966dd072fcb3d9457173dfb3d92
Rhoxolan <pba17mail@gmail.com>
2023-04-19 20:00:44 +0300

    Changed styles; px on font-size were chenged to rem

--- Structured Cabling Studio/wwwroot/js/_Layout.js
+++ Structured Cabling Studio/wwwroot/js/_Layout.js
@@ -22,37 +22,48 @@ function brandDisplayFSChanger() {
     let brandDiv = document.getElementById("brandDisplay");
     let windowWidth = window.innerWidth;
     if (windowWidth > 1399) {
-        brandDiv.style.fontSize = "26px";
+        //brandDiv.style.fontSize = "26px";
+        brandDiv.style.fontSize = "1.7rem";
     }
     else if (windowWidth > 1199) {
-        brandDiv.style.fontSize = "25px";
+        //brandDiv.style.fontSize = "25px";
+        brandDiv.style.fontSize = "1.6rem";
     }
     else if (windowWidth > 575) {
-        brandDiv.style.fontSize = "24px";
+        //brandDiv.style.fontSize = "24px";
+        brandDiv.style.fontSize = "1.5rem";
     }
     else if (windowWidth > 399) {
-        brandDiv.style.fontSize = "19px";
+        //brandDiv.style.fontSize = "19px";
+        brandDiv.style.fontSize = "1.3rem";
     }
     else if (windowWidth > 359) {
-        brandDiv.style.fontSize = "17px";
+        //brandDiv.style.fontSize = "17px";
+        brandDiv.style.fontSize = "1.25rem";
     }
     else if (windowWidth > 319) {
-        brandDiv.style.fontSize = "14.5px";
+        //brandDiv.style.fontSize = "14.5px";
+        brandDiv.style.fontSize = "1.05rem";
     }
     else if (windowWidth > 299) {
-        brandDiv.style.fontSize = "13.1px";
+        //brandDiv.style.fontSize = "13.1px";
+        brandDiv.style.fontSize =  "0.9rem";
     }
     else if (windowWidth > 279) {
-        brandDiv.style.fontSize = "11.5px";
+        //brandDiv.style.fontSize = "11.5px";
+        brandDiv.style.fontSize = "0.8rem";
     }
     else if (windowWidth > 259) {
-        brandDiv.style.fontSize = "9.5px";
+        //brandDiv.style.fontSize = "9.5px";
+        brandDiv.style.fontSize = "0.7rem";
     }
     else if (windowWidth > 239) {
-        brandDiv.style.fontSize = "8.3px";
+        //brandDiv.style.fontSize = "8.3px";
+        brandDiv.style.fontSize = "0.6rem";
     }
     else if (windowWidth > 0) {
-        brandDiv.style.fontSize = "7.3px";
+        //brandDiv.style.fontSize = "7.3px";
+        brandDiv.style.fontSize = "0.55rem";
     }
 }
 
@@ -66,22 +77,28 @@ window.addEventListener('load', verticalSiteNavbarUlFSChanger);
 function verticalSiteNavbarUlFSChanger() {
     let navLinks = document.querySelectorAll('.navLinkText');
     if (window.innerWidth > 1399) {
-        navLinks.forEach(n => n.style.fontSize = '15.5px');
+        //navLinks.forEach(n => n.style.fontSize = '15.5px');
+        navLinks.forEach(n => n.style.fontSize = '1rem');
     }
     else if (window.innerWidth > 991) {
-        navLinks.forEach(n => n.style.fontSize = '15px');
+        //navLinks.forEach(n => n.style.fontSize = '15px');
+        navLinks.forEach(n => n.style.fontSize = '1rem');
     }
     else if (window.innerWidth > 785) {
-        navLinks.forEach(n => n.style.fontSize = '13.5px');
+        //navLinks.forEach(n => n.style.fontSize = '13.5px');
+        navLinks.forEach(n => n.style.fontSize = '0.9rem');
     }
     else if (window.innerWidth > 575) {
-        navLinks.forEach(n => n.style.fontSize = '12.5px');
+        //navLinks.forEach(n => n.style.fontSize = '12.5px');
+        navLinks.forEach(n => n.style.fontSize = '0.9rem');
     }
     else if (window.innerWidth > 319) {
-        navLinks.forEach(n => n.style.fontSize = '12px');
+        //navLinks.forEach(n => n.style.fontSize = '12px');
+        navLinks.forEach(n => n.style.fontSize = '0.85rem');
     }
     else if (window.innerWidth > 0) {
-        navLinks.forEach(n => n.style.fontSize = '8.1px');
+        //navLinks.forEach(n => n.style.fontSize = '8.1px');
+        navLinks.forEach(n => n.style.fontSize = '0.7rem');
     }
 };
 
@@ -96,15 +113,19 @@ window.addEventListener('load', footerFSChanger);
 function footerFSChanger() {
     let footer = document.getElementById('siteFooterText');
     if (window.innerWidth > 1399) {
-        footer.style.fontSize = '17px';
+        //footer.style.fontSize = '17px';
+        footer.style.fontSize = '1rem';
     }
     else if (window.innerWidth > 575) {
-        footer.style.fontSize = '16px';
+        //footer.style.fontSize = '16px';
+        footer.style.fontSize = '1rem';
     }
     else if (window.innerWidth > 319) {
-        footer.style.fontSize = '15px';
+        //footer.style.fontSize = '15px';
+        footer.style.fontSize = '1rem';
     }
     else if (window.innerWidth > 0) {
-        footer.style.fontSize = '12.5px';
+        //footer.style.fontSize = '12.5px';
+        footer.style.fontSize = '0.9rem';
     }
 };
