f2bf3657afe599b343c9a3f6cfe7fa73d9302118
Rhoxolan <pba17mail@gmail.com>
2023-04-25 10:48:20 +0300

    Work on the custom model binder is ongoing

---
+++ Structured Cabling Studio/Binders/CalculationBinders/StructuredCablingStudioParametersModelBinder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using StructuredCablingStudioCore.Parameters;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace StructuredCablingStudio.Binders.CalculationBinders
+{
+	public class StructuredCablingStudioParametersModelBinder : IModelBinder
+	{
+		public Task BindModelAsync(ModelBindingContext bindingContext)
+		{
+			if (bindingContext == null)
+			{
+				throw new ArgumentNullException(nameof(bindingContext));
+			}
+			string sessionKey = "parameters";
+			if(bindingContext.HttpContext.Session != null)
+			{
+				string? data = bindingContext.HttpContext.Session.GetString(sessionKey);
+				if (data != null)
+				{
+					var options = new JsonSerializerOptions
+					{
+						WriteIndented = true,
+						ReferenceHandler = ReferenceHandler.Preserve
+					};
+					(bool? IsStrictСomplianceWithTheStandart,
+					bool? IsAnArbitraryNumberOfPorts,
+					bool? IsTechnologicalReserveAvailability,
+					bool? IsRecommendationsAvailability,
+					double TechnologicalReserve,
+					RecommendationsArguments RecommendationsArguments)? deserializableParameters
+					= JsonSerializer.Deserialize<(bool?, bool?, bool?, bool?, double, RecommendationsArguments)>(data, options);
+					StructuredCablingStudioParameters parameters = new();
+				}
+			}
+			throw new NotImplementedException();
+		}
+	}
+}
