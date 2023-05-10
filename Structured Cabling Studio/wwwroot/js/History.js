f761bf08638a12b40172279fa75019e1ed6703e3
Rhoxolan <pba17mail@gmail.com>
2023-05-10 13:19:38 +0300

    Work on the History page is ongoing

--- Structured Cabling Studio/wwwroot/js/History.js
+++ Structured Cabling Studio/wwwroot/js/History.js
@@ -1,6 +1,12 @@
 loadConfigurationsList();
 loadConfigurationDisplay();
 
+document.addEventListener('click', e => {
+    if (e.target.classList.contains("configurationsListLi")) {
+        loadConfigurationDisplayById(e.target.dataset.id);
+    }
+})
+
 async function loadConfigurationsList() {
     try {
         let resp = await fetch("/Calculation/LoadConfigurationsList", {
@@ -37,3 +43,24 @@ async function loadConfigurationDisplay() {
     }
 }
 
+async function loadConfigurationDisplayById(id) {
+    document.querySelectorAll('.historyPageButton').forEach(b => b.setAttribute('disabled', 'disabled'));
+    try {
+        let resp = await fetch("/Calculation/LoadConfigurationDisplayById", {
+            method: "PUT",
+            body: id
+        });
+        if (resp.ok === true) {
+            let configurationDisplay = await resp.text();
+            document.getElementById('configurationHistoryDisplayDiv').innerHTML = configurationDisplay;
+        }
+        else {
+            alert("Data loading error!");
+        }
+    } catch {
+        alert("Data loading error!");
+    }
+    finally {
+        document.querySelectorAll('.historyPageButton').forEach(b => b.removeAttribute('disabled'));
+    }
+}
