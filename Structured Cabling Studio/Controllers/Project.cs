98d51ca3a5f91168c39d9d317b38e376a92e9d90
Rhoxolan <pba17mail@gmail.com>
2023-05-13 20:19:23 +0300

    Work on the Project Information page is ongoing

---
+++ Structured Cabling Studio/Controllers/Project.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StructuredCablingStudio.Controllers
+{
+	public class Project : Controller
+	{
+		public IActionResult ProjectInformation()
+		{
+			return View();
+		}
+	}
+}
